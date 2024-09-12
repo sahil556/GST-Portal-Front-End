f422a64585a457047d67eea1765b6e1291a0dc38
snandaniya <snandaniya@hhaexchange.com>
2024-09-12 09:29:59 +0530

    Logout Functionality Added to Footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  user: any = '';
+  isLoggedin$!: Observable<boolean>;
+  shouldDisplayLogin: boolean = true;
+  constructor(private authservice: AuthService) {
+  }
+  ngOnInit() {
+    this.isLoggedin$ = this.authservice.isLoggedin();
+  }
 
+  shoulddisplay() {
+    if (window.location.href.endsWith('login') || window.location.href.endsWith('signup')) {
+      return false;
+    }
+    return true;
+  }
+
+  getuser() {
+    return JSON.parse(localStorage.getItem('user') || '{}').email;
+  }
+
+  onLogout() {
+    this.authservice.logout();
+  }
 }
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -8,25 +8,7 @@ import { Observable } from 'rxjs';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  user: any = '';
-  isLoggedin$!: Observable<boolean>;
-  shouldDisplayLogin: boolean = true;
-  constructor(private authservice: AuthService) {
-  }
-  ngOnInit() {
-    this.isLoggedin$ = this.authservice.isLoggedin();
-  }
 
-  shoulddisplay() {
-    if (window.location.href.endsWith('login') || window.location.href.endsWith('signup')) {
-      return false;
-    }
-    return true;
-  }
-
-  getuser() {
-    return JSON.parse(localStorage.getItem('user') || '{}').email;
-  }
   onLogout() {
     // this.authservice.logout();
   }
