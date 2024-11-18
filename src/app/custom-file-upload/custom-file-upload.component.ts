a6faabbb6e3130daa93ba8f6f0d562bf02ce3d47
snandaniya <snandaniya@hhaexchange.com>
2024-11-18 11:43:23 +0530

    custom file upload updated

--- src/app/custom-file-upload/custom-file-upload.component.ts
+++ src/app/custom-file-upload/custom-file-upload.component.ts
@@ -4,26 +4,40 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-custom-file-upload',
   templateUrl: './custom-file-upload.component.html',
-  styleUrl: './custom-file-upload.component.css'
+  styleUrl: './custom-file-upload.component.css',
 })
 export class CustomFileUploadComponent {
-  @Input() Label = ""
-  @Input() fieldName = ""
-  @Input() fileUploadProgress = 0
-  @Input() selectedFile? : File
-  @Input() control: FormControl | undefined
-  @Output() uploadImage = new EventEmitter<string>();
-  @Output() fileChoosen = new EventEmitter<any>();
+  @Input() control!: FormControl; // FormControl to bind input value
+  @Input() label!: string; // Label for the input field
+  @Input() accept!: string; // Accepted file types
+  @Input() errorMessages!: { [key: string]: string }; // Validation error messages
+  @Input() maxFileSizeAllowedInMB!: number; // Max file size allowed
+  @Input() restrictionMessage: string = 'Only JPEG / PNG less than 2MB';
+  @Input() formControlName!: string;
 
-  OnUploadImage($event: Event)
-  {
-    let node  = ($event.target as HTMLButtonElement).name;
-    this.uploadImage.emit(node);
-  }
+  @Output() fileChange = new EventEmitter<any>(); // Emit selected file
 
-  OnFileChoosen($event: { target: any})
-  {
-    let node  = ($event.target as HTMLInputElement).name;
-    this.fileChoosen.emit($event)
+  @Input() documentUploadStatus: string | null = null;
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    console.log(file.type);
+    if (file) {
+      if (file.size > this.maxFileSizeAllowedInMB * 1024 * 1024) {
+        this.control.setErrors({ fileTooLarge: true });
+        this.documentUploadStatus = `File exceeds ${this.maxFileSizeAllowedInMB} MB`;
+      } else if (
+        !this.accept
+          .split(', ')
+          .some((type: string) => file.type.includes(type))
+      ) {
+        this.control.setErrors({ fileFormatInvalid: true });
+        this.documentUploadStatus = 'Invalid File Format!';
+      } else {
+        this.control.setErrors(null);
+        this.documentUploadStatus = null;
+        this.fileChange.emit(file); // Emit the selected file
+      }
+    }
   }
 }
