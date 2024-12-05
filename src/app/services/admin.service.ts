6b40a6b4ef4e9bb8891823aa24385c604a618640
snandaniya <snandaniya@hhaexchange.com>
2024-12-05 22:08:46 +0530

    Navigate To Login page on unauthorized Access

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -4,36 +4,38 @@ import { firstValueFrom, map } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { GstForm } from '../models/gst-form';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
-  constructor(private http: HttpClient, private toastr: ToastrService) { }
-
-  loadDataFromFirestore()
-  {
-    console.log("Is Production")
-    console.log(environment.isProduction)
+  loadDataFromFirestore() {
+    console.log('Is Production');
+    console.log(environment.isProduction);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-
     let responce = firstValueFrom(
-      this.http.get <Array<GstForm>>(environment.baseUrl + '/GSTDetails', { headers })
+      this.http.get<Array<GstForm>>(environment.baseUrl + '/GSTDetails', {
+        headers,
+      })
     );
     responce
-      .then((logRef: any) => {
-      })
+      .then((logRef: any) => {})
       .catch((err) => {
         if (err.status == 401) {
-          this.toastr.warning(
-            'Session Time out',
-            'Please Login Again !'
-          );
+          this.toastr.warning('Session Time out', 'Please Login Again !');
+          localStorage.removeItem('token')
+          localStorage.removeItem('refreshToken')
+          this.router.navigate(['/login']);
         } else this.toastr.warning(err.statusText, 'Some thing went Wrong !');
       });
     return responce;
   }
-
 }
